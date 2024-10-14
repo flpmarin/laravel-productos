903dcdb5d65d25537fb33e9efc830c33cb72c8a6
felipe_marin_st <flpmarin@digitechfp.com>
2024-10-15 00:08:20 +0200

    create cycle

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,6 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-    hola
+    <h3>Productos</h3>
+    <form action="{{ route('store')}}" method="post">
+        @csrf
+        <input type="text" name="name" placeholder="name"/>
+        <input type="text" name="description" placeholder="description"/>
+        <input type="number" name="price" placeholder="price"/>     
+        <input type="submit" value="agregar producto" /> 
+    </form>
+
+    <h1>Productos:</h1>
+    <ul>
+        @forelse ($products as $product)
+        <li>{{ $product->name }}</li>
+        @empty
+        <li>No data</li>
+        @endforelse
+    </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\ProductController::class,'index'])->name('index');
+Route::get('/',[App\Http\Controllers\ProductController::class,'index'])->name('index');
+Route::get('/create',[App\Http\Controllers\ProductController::class,'create'])->name('create');
+Route::post('/store',[App\Http\Controllers\ProductController::class,'store'])->name('store');
